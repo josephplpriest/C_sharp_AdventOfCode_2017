e98cbdd71ea8815807c4660f2a561e21607c8ae2
Paul Priest <josephplpriest@gmail.com>
2024-03-14 12:07:32 -0400

    day 4 pt 1

---
+++ day_4/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string filePath = "input/input.txt";
+
+string[] lines = File.ReadAllLines(filePath);
+
+int distinctLineCounter = 0;
+
+foreach (string line in lines) {
+    int distinctCount = line.Split().ToList().Distinct().Count();
+    int nonDistinctCount = line.Split().ToList().Count;
+    if (distinctCount == nonDistinctCount) 
+    {
+        distinctLineCounter += 1;
+    }
+}
+
+Console.WriteLine(distinctLineCounter);
